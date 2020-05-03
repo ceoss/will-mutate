439a9347e2d93a6a12f6bee97e66f25fb916cfe6
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-03 11:56:32 -0700

    Ninja away variable names Sorry Eve

---
+++ src/foo.js
@@ -0,0 +1,20 @@
+const global = {prop: "test"};
+
+$shouldNotMutate(foo);
+const foo = (foo) => {
+	foo.prop = "pie";
+};
+
+$shouldNotMutate(foo);
+function bar(foo) {
+	foo.prop = 'Test';
+}
+
+/**
+ * This does not currently work
+ */
+$shouldNotMutate(foo);
+const pizza = foo => console.log(foo);
+
+foo(global);
+bar(global);
