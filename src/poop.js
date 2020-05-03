8b3dbc27202f1f76ce898bd1c572c5a81ff96aaa
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-02 22:25:22 -0700

    Added initial detection of JS body, also initial work on proxy code

--- src/poop.js
+++ src/poop.js
@@ -1,6 +1,17 @@
-const poop = {prop: "test"};
+const global = {prop: "test"};
 
-shouldNotMutate(poop);
-const foo = () => {
+$shouldNotMutate(poop);
+const foo = (poop) => {
 	poop.prop = "pie";
 };
+
+$shouldNotMutate(poop);
+function bar(poop) {
+	poop.prop = 'Test';
+}
+
+$shouldNotMutate(poop);
+const pizza = a => console.log(a);
+
+foo(global);
+bar(global);
