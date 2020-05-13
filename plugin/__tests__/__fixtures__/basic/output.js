cff66e06ee6b66439d9b6df730bbba9ebde94527
Evelyn Hathaway <git@evelyn.dev>
2020-05-12 22:51:04 -0700

    Update: Adding setter and getter apply traps, fixing bugs, more tests

--- plugin/__tests__/__fixtures__/basic/output.js
+++ plugin/__tests__/__fixtures__/basic/output.js
@@ -1,3 +1,4 @@
+// Create property paths that mostly match the limitations of dot notation
 const propPath = function (path, property) {
   if (/^[$A-Z_a-z][\w$]*$/.test(property)) {
     return `${path}.${property}`;
@@ -15,8 +16,11 @@ const _will_mutate_check_proxify = (target, options = {}) => {
 
   const {
     deep = false,
-    prototype = false
-  } = options; // Naming properties for mutation tracing in errors
+    prototype = false,
+    _isSetter = false,
+    _isGetter = false
+  } = options;
+  const isShallowSetter = _isSetter && !deep; // Naming properties for mutation tracing in errors
 
   const {
     name = typeof target.name === "string" && target.name
@@ -24,86 +28,83 @@ const _will_mutate_check_proxify = (target, options = {}) => {
   let {
     path = "target"
   } = options;
-  if (name !== "undefined" && name !== false) path = propPath(path, name); // If the proxy trap was triggered by the function to test
-  // TODO: [>=1] evaluate for v1 - implement, possibly make optional?
+  if (name !== undefined && name !== false) path = propPath(path, name); // Options for resursive function
 
-  const triggeredByFunction = true; // Proxy handler
+  const recursiveOptions = { // Extend existing options
+    ...options,
+    // Add new path
+    path,
+    // Reset temporary internal flags
+    _isSetter: false,
+    _isGetter: false
+  }; // Proxy handler
 
-  const handler = {
-    // Accessor edge case traps
-    getOwnPropertyDescriptor(dummyTarget, prop) {
-      /*
-      	Early return for cached read-only properties, prevents the below invariant when adding read-only properties to the dummy:
-      	"The result of Object.getOwnPropertyDescriptor(target) can be applied to the target object using Object.defineProperty() and will not throw an exception."
-      */
-      const dummyDescriptor = Reflect.getOwnPropertyDescriptor(...arguments);
-      if (dummyDescriptor) return dummyDescriptor; // Reflect using the real target, not the dummy
+  const handler = {}; // Get traps for deep mutation assertions
+  // Accessor edge case traps
 
-      const reflectArguments = [...arguments];
-      reflectArguments[0] = target;
-      const descriptor = Reflect.getOwnPropertyDescriptor(...reflectArguments); // Early return for non-existing properties
+  handler.getOwnPropertyDescriptor = function (dummyTarget, prop) {
+    /*
+    	Early return for cached read-only properties, prevents the below invariant when adding read-only properties to the dummy:
+    	"The result of Object.getOwnPropertyDescriptor(target) can be applied to the target object using Object.defineProperty() and will not throw an exception."
+    */
+    const dummyDescriptor = Reflect.getOwnPropertyDescriptor(...arguments);
+    if (dummyDescriptor) return dummyDescriptor; // Reflect using the real target, not the dummy
 
-      if (!descriptor) return; // If has a value instead of accessors
+    const reflectArguments = [...arguments];
+    reflectArguments[0] = target;
+    const descriptor = Reflect.getOwnPropertyDescriptor(...reflectArguments); // Early return for non-existing properties
 
-      const isValueDesc = ("value" in descriptor);
+    if (!descriptor) return; // If has a value instead of accessors
 
-      if (deep) {
-        if (isValueDesc) {
-          descriptor.value = _will_mutate_check_proxify(descriptor.value, { ...options,
-            path,
-            name: prop
-          });
-        } else {// descriptor.set = _will_mutate_check_proxify(descriptor.set, {...options, path, name: prop}); // TODO: [>=1] before publishing stable - add apply traps
-          // descriptor.get = _will_mutate_check_proxify(descriptor.get, {...options, path, name: prop}); // TODO: [>=1] before publishing stable - add apply traps
-        }
-      } else if (!isValueDesc) {} // descriptor.set = descriptor.set && new Proxy(descriptor.set, descriptorSetHandler); // TODO: [>=1] before publishing stable - add apply traps
+    const isValueDesc = ("value" in descriptor);
 
-      /*
-      	Add read-only props to `dummyTarget` to meet the below invariant:
-      	"A property cannot be reported as existent, if it does not exists as an own property of the target object and the target object is not extensible."
-      */
-
-
-      const isReadOnly = descriptor.writable === false || descriptor.configurable === false;
-      if (isReadOnly) Object.defineProperty(dummyTarget, prop, descriptor);
-      return descriptor;
+    if (deep) {
+      if (isValueDesc) {
+        descriptor.value = _will_mutate_check_proxify(descriptor.value, { ...recursiveOptions,
+          name: prop
+        });
+      } else {
+        descriptor.get = _will_mutate_check_proxify(descriptor.get, { ...recursiveOptions,
+          name: `get(${prop})`,
+          isGetter: true
+        });
+      }
     }
 
-  }; // Reflect to the real target for unused traps
-  // This is to avoid the navtive fallback to the `dummyTarget`
+    if (!isValueDesc) {
+      descriptor.set = _will_mutate_check_proxify(descriptor.set, { ...recursiveOptions,
+        name: `set(${prop})`,
+        isSetter: true
+      });
+    }
+    /*
+    	Add read-only props to `dummyTarget` to meet the below invariant:
+    	"A property cannot be reported as existent, if it does not exists as an own property of the target object and the target object is not extensible."
+    */
 
-  const addNoopReflectUsingRealTargetTrap = trap => {
-    handler[trap] = function () {
-      // Reflect using the real target, not the dummy
-      const reflectArguments = [...arguments];
-      reflectArguments[0] = target;
-      return Reflect[trap](...reflectArguments);
-    };
+
+    const isReadOnly = descriptor.writable === false || descriptor.configurable === false;
+    if (isReadOnly) Object.defineProperty(dummyTarget, prop, descriptor);
+    return descriptor;
   };
 
-  addNoopReflectUsingRealTargetTrap("isExtensible");
-  addNoopReflectUsingRealTargetTrap("has");
-  addNoopReflectUsingRealTargetTrap("ownKeys");
-  addNoopReflectUsingRealTargetTrap("apply");
-  addNoopReflectUsingRealTargetTrap("construct"); // Getting traps for deep mutation assertions
-
-  const addDeepGetTrap = trap => {
+  const addGetTrap = trap => {
     handler[trap] = function (dummyTarget, prop) {
       // Reflect using the real target, not the dummy
       const reflectArguments = [...arguments];
       reflectArguments[0] = target;
       if (trap === "getPrototypeOf") prop = "__proto__";
       const real = Reflect[trap](...reflectArguments);
-      return _will_mutate_check_proxify(real, { ...options,
-        path,
+      return deep || _isGetter ? _will_mutate_check_proxify(real, { ...recursiveOptions,
         name: prop
-      });
+      }) : real; // Will revert to the actual target if not deep
     };
   };
 
-  deep && addDeepGetTrap("get"); // Covered by getOwnPropertyDescriptor, but is more specific
+  addGetTrap("get"); // Covered by getOwnPropertyDescriptor, but is more specific
 
-  prototype && addDeepGetTrap("getPrototypeOf"); // Mutation traps for erroring
+  prototype && addGetTrap("getPrototypeOf");
+  _isGetter && addGetTrap("apply"); // Mutation traps for erroring
 
   const addSetTrap = trap => {
     handler[trap] = function (dummyTarget, prop) {
@@ -116,17 +117,39 @@ const _will_mutate_check_proxify = (target, options = {}) => {
         path += `.${prop}`;
       }
 
-      if (triggeredByFunction) throw new Error(`Mutation assertion failed. \`${trap}\` trap triggered on \`${path}\`.`);
+      throw new Error(`Mutation assertion failed. \`${trap}\` trap triggered on \`${path}\`.`);
+    };
+  };
+
+  if (!isShallowSetter) {
+    addSetTrap("set"); // Covered by defineProperty, but is more specific
+
+    addSetTrap("defineProperty");
+    addSetTrap("deleteProperty");
+    addSetTrap("preventExtensions");
+    prototype && addSetTrap("setPrototypeOf");
+  }
+
+  _isSetter && addSetTrap("apply"); // Reflect to the real target for unused traps
+  // This is to avoid the navtive fallback to the `dummyTarget`
+
+  const addNoopReflectUsingRealTargetTrap = trap => {
+    // Early return for existing traps
+    if (handler[trap]) return;
+
+    handler[trap] = function () {
+      // Reflect using the real target, not the dummy
+      const reflectArguments = [...arguments];
+      reflectArguments[0] = target;
       return Reflect[trap](...reflectArguments);
     };
   };
 
-  addSetTrap("set"); // Covered by defineProperty, but is more specific
-
-  addSetTrap("defineProperty");
-  addSetTrap("deleteProperty");
-  prototype && addSetTrap("setPrototypeOf");
-  addSetTrap("preventExtensions"); // Don't use the true `target` as the proxy target to avoid issues with read-only types
+  addNoopReflectUsingRealTargetTrap("isExtensible");
+  addNoopReflectUsingRealTargetTrap("has");
+  addNoopReflectUsingRealTargetTrap("ownKeys");
+  addNoopReflectUsingRealTargetTrap("apply");
+  addNoopReflectUsingRealTargetTrap("construct"); // Don't use the true `target` as the proxy target to avoid issues with read-only types
   // Create `dummyTarget` based on the `target`'s constructor
 
   const dummyTarget = new (Object.getPrototypeOf(target).constructor)();
--- plugin/__tests__/proxy.js
+++ plugin/__tests__/proxy.js
@@ -2,9 +2,10 @@ const proxify = require("../proxy");
 
 
 describe("Proxy util", () => {
-	let target;
+	const targets = {};
+	let nakedTarget;
 	beforeEach(() => {
-		const nakedTarget = Object.assign(
+		nakedTarget = Object.assign(
 			Object.create({protoProp: "old"}),
 			{
 				prop: "old",
@@ -23,27 +24,84 @@ describe("Proxy util", () => {
 			set() {},
 			get() {return {};},
 		});
-		target = proxify(
+
+		targets.deepProto = proxify(
 			nakedTarget,
-			{deep: true, prototype: true}
+			{deep: true, prototype: true},
 		);
+		targets.deep = proxify(
+			nakedTarget,
+			{deep: true, prototype: false},
+		);
+		targets.proto = proxify(
+			nakedTarget,
+			{deep: false, prototype: true},
+		);
+		targets.shallow = proxify(
+			nakedTarget,
+			{deep: false, prototype: false},
+		);
+		targets.all = [
+			targets.shallow,
+			targets.deep,
+			targets.proto,
+			targets.deepProto,
+		];
 	});
+
+
+	/*
+		Basic mutation failure
+	*/
 	test("to throw when assigning a prop", async () => {
-		expect(() => {
-			target.prop = "New";
-		}).toThrow("Mutation assertion failed. `set` trap triggered on `target.prop`.");
+		targets.all.forEach((target) => {
+			expect(() => {
+				target.prop = "New";
+			}).toThrow("Mutation assertion failed. `set` trap triggered on `target.prop`.");
+		});
 	});
+
+	/*
+		Assert that the dummy target and getting is working okie dokie
+	*/
 	test("to not throw when accessing read-only props multiple times", async () => {
+		targets.all.forEach((target) => {
+			expect(() => {
+				target.readOnly;
+				target.readOnly;
+				Object.getOwnPropertyDescriptor(target, "readOnly");
+				Object.getOwnPropertyDescriptor(target, "readOnly");
+				Object.getOwnPropertyDescriptor(target, "accessor");
+				Object.getOwnPropertyDescriptor(target, "accessor");
+				new target.classy();
+				new target.classy();
+			}).not.toThrow();
+		});
+	});
+
+
+	/*
+		getOwnPropertyDescriptor edge cases
+	*/
+	test("to throw when using a setter via getOwnPropertyDescriptor", async () => {
 		expect(() => {
-			target.readOnly;
-			target.readOnly;
-			Object.getOwnPropertyDescriptor(target, "readOnly");
-			Object.getOwnPropertyDescriptor(target, "readOnly");
-			Object.getOwnPropertyDescriptor(target, "accessor");
-			Object.getOwnPropertyDescriptor(target, "accessor");
-			new target.classy();
-			new target.classy();
+			Object.getOwnPropertyDescriptor(targets.deep, "accessor").set("new");
+		}).toThrow();
+	});
+	test("to throw when setting after getting via getOwnPropertyDescriptor", async () => {
+		expect(() => {
+			Object.getOwnPropertyDescriptor(targets.deep, "accessor").get().prop = "new";
+		}).toThrow();
+	});
+	test("to not throw when setting a new prop on an accessor via getOwnPropertyDescriptor (shallow)", async () => {
+		expect(() => {
+			Object.getOwnPropertyDescriptor(targets.shallow, "accessor").set.prop = "new";
 		}).not.toThrow();
+	});
+	test("to throw when setting a new prop on an accessor via getOwnPropertyDescriptor", async () => {
+		expect(() => {
+			Object.getOwnPropertyDescriptor(targets.deep, "accessor").set.prop = "new";
+		}).toThrow("Mutation assertion failed. `set` trap triggered on `target[\"set(accessor)\"].prop`.");
 	});
 });
 
@@ -54,9 +112,7 @@ describe("Proxy util", () => {
 // Object.defineProperty(target, "prop", {value: "new"}); // Errors
 // Object.getPrototypeOf(target).protoProp = "new"; // Errors when `prototype`
 // Object.getOwnPropertyDescriptor(target, "obj").value.prop = "new"; // Errors when `deep`;
-// Object.getOwnPropertyDescriptor(target, "accessor").set("new"); // TODO: [>=1] before publishing stable - make error (accessor apply trap proxying)
-// Object.getOwnPropertyDescriptor(target, "accessor").set.prop = "new"; // TODO: [>=1] before publishing stable - make error (accessor apply trap proxying)
-// Object.getOwnPropertyDescriptor(target, "accessor").get().prop = "new"; // TODO: [>=1] before publishing stable - make error (accessor apply trap proxying)
 // Object.setPrototypeOf(target, {}); // Errors when `prototype`
 // Object.preventExtensions(target.prop); // Errors
 // TODO: [>=1] before publishing stable - add tests to find the limitations of the proxy, dummytarget, etc.
+// TODO: [>=1] before publishing stable - add inverted tests
